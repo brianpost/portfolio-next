d7041daba6d0958de4a8bc30224e94baa39631a6
brianpost <brianmpost@gmail.com>
2022-06-19 16:27:24 -0400

    Add Contact component and SendGrid api route, adjust page column component, readme text updates, set project image priority

---
+++ components/Contact.tsx
@@ -0,0 +1,151 @@
+
+import React, {useState} from "react";
+
+enum ToastType {
+    Success,
+    Failure
+}
+
+const Contact = () => {
+    const [fullName, setFullName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [subject, setSubject] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
+    const [isSending, setIsSending] = useState<boolean>(false)
+    const [showSuccessMessage, setShowSuccessMessage] = useState<boolean>(false)
+    const [showFailureMessage, setShowFailureMessage] = useState<boolean>(false)
+
+    const handleSend = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setIsSending(true)
+        setShowSuccessMessage(false)
+        setShowFailureMessage(false)
+        const res = await fetch("/api/sendgrid", {
+            body: JSON.stringify({
+                fullName,
+                email,
+                subject,
+                message
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+        })
+        const { error } = await res.json();
+        setIsSending(false)
+        if (error) {
+            console.error(error)
+            setShowSuccessMessage(false)
+            setShowFailureMessage(true)
+            return;
+        }
+        setFullName('')
+        setEmail('')
+        setSubject('')
+        setMessage('')
+        setShowSuccessMessage(true)
+        setShowFailureMessage(false)
+    }
+
+    const AfterSendToast = ({type, children} : {type: ToastType, children: React.ReactNode}) => {
+        let textBackgroundClassName
+        switch (type) {
+            case ToastType.Success:
+                textBackgroundClassName = 'text-bg-success'
+                break
+            case ToastType.Failure:
+                textBackgroundClassName = 'text-bg-danger'
+                break
+        }
+        return (
+            <div
+                className={`toast align-items-center border border-1 border-light position-fixed top-0 end-0 m-5 show ${textBackgroundClassName}`}
+                role="alert"
+                aria-live="assertive"
+                aria-atomic="true"
+            >
+                <div className="d-flex">
+                    <div className="toast-body">
+                        {children}
+                    </div>
+                    <button
+                        type="button"
+                        className="btn-close btn-close-white me-2 m-auto"
+                        data-bs-dismiss="toast"
+                        aria-label="Close"
+                        onClick={()=>{
+                            setShowSuccessMessage(false)
+                            setShowFailureMessage(false)
+                        }}
+                    />
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <form onSubmit={handleSend}>
+            <h2 className="h3">Contact me</h2>
+            <div className="my-3">
+                <label htmlFor="inputContactFullName" className="form-label">Full Name</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="inputContactFullName"
+                    required={true}
+                    value={fullName}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setFullName(event.target.value)}
+                />
+            </div>
+            <div className="my-3">
+                <label htmlFor="inputContactEmail" className="form-label">Email</label>
+                <input
+                    type="email"
+                    className="form-control"
+                    id="inputContactEmail"
+                    required={true}
+                    value={email}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+                />
+            </div>
+            <div className="my-3">
+                <label htmlFor="inputContactSubject" className="form-label">Subject</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="inputContactSubject"
+                    required={true}
+                    value={subject}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setSubject(event.target.value)}
+                />
+            </div>
+            <div className="my-3">
+                <label htmlFor="inputContactMessage" className="form-label">Message</label>
+                <textarea
+                    className="form-control"
+                    id="inputContactMessage"
+                    rows={4}
+                    required={true}
+                    value={message}
+                    onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(event.target.value)}
+                />
+            </div>
+            <div className="my-3 text-end">
+                <input type="submit" className="btn" value={isSending ? 'Sending...' : 'Send message'} disabled={isSending}/>
+            </div>
+            {showSuccessMessage &&
+                <AfterSendToast type={ToastType.Success}>
+                    Thank you! Your message has been delivered.
+                </AfterSendToast>
+            }
+            {showFailureMessage &&
+                <AfterSendToast type={ToastType.Failure}>
+                    Oops! Something went wrong sending your message, please try again.
+                </AfterSendToast>
+            }
+        </form>
+    )
+}
+
+export default Contact
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
+import Contact from "../components/Contact";
 
 const Home: NextPage = () => {
 
@@ -32,7 +33,10 @@ const Home: NextPage = () => {
         )
     }
 
-    const ProjectCard = ({title, imageName, children}: {title: string, imageName: string, children: React.ReactNode}) => {
+    const ProjectCard = (
+        {title, imageName, children, imagePriority = false}:
+            {title: string, imageName: string, children: React.ReactNode, imagePriority?: boolean}
+    ) => {
 
         const ProjectCardHeading = () => {
             return (
@@ -46,6 +50,7 @@ const Home: NextPage = () => {
             return (
                 <div className="project-image-container">
                     <Image
+                        priority={imagePriority}
                         width={600}
                         height={400}
                         src={`/projects/${imageName}`}
@@ -71,8 +76,10 @@ const Home: NextPage = () => {
 
     const PageColumn = ({children}: {children: React.ReactNode}) => {
         return (
-            <div className="col-sm-12 col-md-10 col-lg-7">
-                {children}
+            <div className="row justify-content-center">
+                <div className="col-sm-12 col-md-10 col-lg-7">
+                    {children}
+                </div>
             </div>
         )
     }
@@ -88,117 +95,113 @@ const Home: NextPage = () => {
                     content="Brian Post is a full stack web application developer who builds websites and applications using React, Next.js, TypeScript, PHP, Symfony, and MariaDB"
                 />
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
-                    rel="stylesheet"
-                />
             </Head>
 
             <header>
                 <div className="container my-5">
-                    <div className="row justify-content-center">
-                        <PageColumn>
-                            <div className="d-flex flex-sm-row flex-column justify-content-center text-center text-sm-start">
-                                <div className="me-3">
-                                    <Image
-                                        priority
-                                        src="/profile.jpg"
-                                        className="rounded-circle"
-                                        height={108}
-                                        width={108}
-                                        alt="Brian Post's profile image"
-                                    />
-                                </div>
-                                <div>
-                                    <h1 className="h2">
-                                        Brian Post
-                                        <small className="text-muted d-block h5">Web Application Developer</small>
-                                    </h1>
-                                    <button className="btn btn-sm btn-outline-secondary d-inline-flex align-items-center">
-                                        <EnvelopeIcon/> Contact me
-                                    </button>
-                                </div>
+                    <PageColumn>
+                        <div className="d-flex flex-sm-row flex-column justify-content-center text-center text-sm-start">
+                            <div className="me-3">
+                                <Image
+                                    priority
+                                    src="/profile.jpg"
+                                    className="rounded-circle"
+                                    height={108}
+                                    width={108}
+                                    alt="Brian Post's profile image"
+                                />
                             </div>
-                        </PageColumn>
-                    </div>
+                            <div>
+                                <h1 className="h2">
+                                    Brian Post
+                                    <small className="text-muted d-block h5">Web Application Developer</small>
+                                </h1>
+                                <a href={'#Contact'} className="btn btn-sm btn-outline-secondary d-inline-flex align-items-center">
+                                    <EnvelopeIcon/> Contact me
+                                </a>
+                            </div>
+                        </div>
+                    </PageColumn>
                 </div>
             </header>
 
             <main>
                 <div className="container">
+
                     <section>
-                        <div className="row justify-content-center">
-                            <PageColumn>
-                                <p>Hello, I&apos;m Brian.  I&apos;m a Full Stack Web Application Developer.</p>
-                                <p>
-                                    My current focus is on building complex websites and applications
-                                    using <ContentTechLink href="https://reactjs.org/">React</ContentTechLink>
-                                    , <ContentTechLink href="https://nextjs.org/">Next.js</ContentTechLink>
-                                    , <ContentTechLink href="https://www.typescriptlang.org/">TypeScript</ContentTechLink>
-                                    , <ContentTechLink href="https://www.php.net/">PHP</ContentTechLink>
-                                    , <ContentTechLink href="https://symfony.com/">Symfony</ContentTechLink>
-                                    , and <ContentTechLink href="https://mariadb.com/">MariaDB</ContentTechLink>.
-                                </p>
-                            </PageColumn>
-                        </div>
+                        <PageColumn>
+                            <p>Hello, I&apos;m Brian.  I&apos;m a Full Stack Web Application Developer.</p>
+                            <p>
+                                My current focus is on building complex websites and applications
+                                using <ContentTechLink href="https://reactjs.org/">React</ContentTechLink>
+                                , <ContentTechLink href="https://nextjs.org/">Next.js</ContentTechLink>
+                                , <ContentTechLink href="https://www.typescriptlang.org/">TypeScript</ContentTechLink>
+                                , <ContentTechLink href="https://www.php.net/">PHP</ContentTechLink>
+                                , <ContentTechLink href="https://symfony.com/">Symfony</ContentTechLink>
+                                , and <ContentTechLink href="https://mariadb.com/">MariaDB</ContentTechLink>.
+                            </p>
+                        </PageColumn>
                     </section>
+
+                    <section className="mt-4 mb-3">
+                        <PageColumn>
+                            <h2 className="h4">Featured Projects</h2>
+                            <p className="">Past projects I&apos;ve developed independently.</p>
+                        </PageColumn>
+                        <PageColumn>
+                            <ProjectCard title="Northwestern University Law Review" imageName="nulr.png" imagePriority={true}>
+                                <p>Built and led the previous redesign of Northwestern University Law Review. Developed using Drupal 7.</p>
+                            </ProjectCard>
+                            <ProjectCard title="SmartParticipation" imageName="smartparticipation.png">
+                                <p>SmartParticipation is a custom discussion platform developed by the Cornell e-Rulemaking Initiative at Cornell University Law School.</p>
+                                <p>I worked as Programmer for the project from 2011-2014, then Lead Technologist from 2014-2017.</p>
+                                <p>The platform is a Drupal 7 distribution, and includes several custom modules.</p>
+                                <p>While the CeRI project has ended, I continue to maintain the open source code.</p>
+                                <a href="http://smartparticipation.com/" className="btn btn-sm btn-secondary">
+                                    View SmartParticipation project site
+                                </a>
+                            </ProjectCard>
+                            <ProjectCard title="Politiki" imageName="politiki.gif">
+                                <p>Politiki breaks down political issues into easy to follow pro/con arguments.</p>
+                                <p>Developed all aspects of site using CakePHP, MySQL, and JavaScript.</p>
+                                <a href="http://politiki.us/" className="btn btn-sm btn-secondary">
+                                    View archived Politiki site
+                                </a>
+                            </ProjectCard>
+                            <ProjectCard title="Lucky Liquors Invoice Manager" imageName="invoicemanager.gif">
+                                <p>Developed application to simplify invoice to inventory data entry.  Integrates with third-party inventory tracking software.</p>
+                                <p>Built using CakePHP, MySQL, and JavaScript.</p>
+                            </ProjectCard>
+                            <ProjectCard title="Boston University Dear Abbeys" imageName="dearabbeys.gif">
+                                <p>Converted earlier version of public site for use with WordPress.  Included blog and content management.</p>
+                                <p>Designed and developed members only website.  This includes access control, alumni profiles, alumni search, message board, newsletter, event management, and more.</p>
+                                <p>The members site was developed using CakePHP, MySQL, and integrates with phpBB, poMMo, and WordPress.</p>
+                            </ProjectCard>
+                        </PageColumn>
+                    </section>
+
                     <section>
-                        <div className="row justify-content-center mt-4">
-                            <PageColumn>
-                                <h2 className="h4">Featured Projects</h2>
-                                <p className="">Past projects I&apos;ve developed independently.</p>
-                            </PageColumn>
-                        </div>
-                        <div className="row justify-content-center mb-5">
-                            <PageColumn>
-                                <ProjectCard title="Northwestern University Law Review" imageName="nulr.png">
-                                    <p>Built and led the previous redesign of Northwestern University Law Review. Developed using Drupal 7.</p>
-                                </ProjectCard>
-                                <ProjectCard title="SmartParticipation" imageName="smartparticipation.png">
-                                    <p>SmartParticipation is a custom discussion platform developed by the Cornell e-Rulemaking Initiative at Cornell University Law School.</p>
-                                    <p>I worked as Programmer for the project from 2011-2014, then Lead Technologist from 2014-2017.</p>
-                                    <p>The platform is a Drupal 7 distribution, and includes several custom modules.</p>
-                                    <p>While the CeRI project has ended, I continue to maintain the open source code.</p>
-                                    <a href="http://smartparticipation.com/" className="btn btn-sm btn-secondary">
-                                        View SmartParticipation project site
-                                    </a>
-                                </ProjectCard>
-                                <ProjectCard title="Politiki" imageName="politiki.gif">
-                                    <p>Politiki breaks down political issues into easy to follow pro/con arguments.</p>
-                                    <p>Developed all aspects of site using CakePHP, MySQL, and JavaScript.</p>
-                                    <a href="http://politiki.us/" className="btn btn-sm btn-secondary">
-                                        View archived Politiki site
-                                    </a>
-                                </ProjectCard>
-                                <ProjectCard title="Lucky Liquors Invoice Manager" imageName="invoicemanager.gif">
-                                    <p>Developed application to simplify invoice to inventory data entry.  Integrates with third-party inventory tracking software.</p>
-                                    <p>Built using CakePHP, MySQL, and JavaScript.</p>
-                                </ProjectCard>
-                                <ProjectCard title="Boston University Dear Abbeys" imageName="dearabbeys.gif">
-                                    <p>Converted earlier version of public site for use with WordPress.  Included blog and content management.</p>
-                                    <p>Designed and developed members only website.  This includes access control, alumni profiles, alumni search, message board, newsletter, event management, and more.</p>
-                                    <p>The members site was developed using CakePHP, MySQL, and integrates with phpBB, poMMo, and WordPress.</p>
-                                </ProjectCard>
-                            </PageColumn>
-                        </div>
+                        <PageColumn>
+                            <div id="Contact" className="contact bg-primary text-light p-4 rounded mb-5">
+                                <Contact/>
+                            </div>
+                        </PageColumn>
                     </section>
+
                 </div>
             </main>
 
             <footer className="bg-black text-white mt-auto">
                 <div className="container">
-                    <div className="row justify-content-center p-1">
-                        <PageColumn>
-                            <div className="text-center">
-                                Built with <FooterTechLink href="https://nextjs.org/">Next.js</FooterTechLink>
-                                , <FooterTechLink href="https://getbootstrap.com/">Bootstrap</FooterTechLink>
-                                , and <FooterTechLink href="https://sendgrid.com/">SendGrid</FooterTechLink>.
-                                <a href="#" className="link-light ms-3">View source</a>
-                            </div>
-                        </PageColumn>
-                    </div>
+                    <PageColumn>
+                        <div className="text-center p-1">
+                            Made with <FooterTechLink href="https://nextjs.org/">Next.js</FooterTechLink>
+                            , <FooterTechLink href="https://www.typescriptlang.org/">TypeScript</FooterTechLink>
+                            , <FooterTechLink href="https://getbootstrap.com/">Bootstrap</FooterTechLink>
+                            , and <FooterTechLink href="https://sendgrid.com/">SendGrid</FooterTechLink>.
+                            <a href="#" className="link-light ms-3">View source</a>
+                        </div>
+                    </PageColumn>
                 </div>
             </footer>
 
